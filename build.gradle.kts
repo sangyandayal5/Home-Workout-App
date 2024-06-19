ce2a3b13d91df12949f76b3798a82893e5a21dc7
Rahul-Nikhate <rahulnikhate706636@gmail.com>
2024-06-19 15:30:54 +0530

    First Commit

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+
+buildscript {
+    dependencies {
+        classpath ("com.google.dagger:hilt-android-gradle-plugin:2.40.1")
+        classpath ("com.google.gms:google-services:4.3.13")
+
+        val nav_version = "2.5.0"
+        classpath ("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+    }
+}
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.1.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+}
