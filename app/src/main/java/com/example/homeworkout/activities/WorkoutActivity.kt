dc8fa7daeb7977ce6212ec3db3059977da691c53
Rahul-Nikhate <rahulnikhate706636@gmail.com>
2024-06-30 23:26:23 +0530

    Name and uid retrival added

--- app/src/main/java/com/example/homeworkout/activities/WorkoutActivity.kt
+++ app/src/main/java/com/example/homeworkout/activities/WorkoutActivity.kt
@@ -1,20 +1,53 @@
 package com.example.homeworkout.activities
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.homeworkout.R
-import com.example.homeworkout.databinding.ActivitySignInBinding
+import android.util.Log
 import com.example.homeworkout.databinding.ActivityWorkoutBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class WorkoutActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivityWorkoutBinding
-    private lateinit var  firebaseAuth: FirebaseAuth
+    private lateinit var binding: ActivityWorkoutBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var uid: String
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding  = ActivityWorkoutBinding.inflate(layoutInflater)
+        binding = ActivityWorkoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+        val db = Firebase.firestore
+        val bundle = intent.extras
+        uid = bundle!!.getString("UID", "-1")
+
+        val currentContext = this  // Workaround to avoid capturing `this` in the lambda
+
+        val docRef = db.collection("users").document(uid)
+        docRef.get().addOnSuccessListener { document ->
+            if (document != null && document.exists()) {
+                val fname = document.data?.get("fname").toString()
+                val lname = document.data?.get("lname").toString()
+                val userName = "$fname $lname"
+                currentContext.cname(userName)  // Use the local context reference
+                Log.d("WorkoutActivity", "data : ${document.data}")
+            } else {
+                Log.d("WorkoutActivity", "No data")
+            }
+        }
+            .addOnFailureListener { exception ->
+                Log.d("WorkoutActivity", "get failed with ", exception)
+            }
+    }
+
+    private fun cname(userName: String) {
+        val displayText = "Hey, $userName"
+        val textView = binding.awUName
+        textView.text = displayText
     }
 }
